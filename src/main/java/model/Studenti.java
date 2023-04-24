5cb589f1d6ba516e9d46e66e936b5206138375bb
Alketa Zekaj <118227007+AlketaZekaj@users.noreply.github.com>
2023-04-24 19:31:52 +0200

    .

--- src/main/java/model/Studenti.java
+++ src/main/java/model/Studenti.java
@@ -1,4 +1,96 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Studenti {
+
+    private int id;
+    private String emri;
+    private String mbiemri;
+    private String studentId;
+    private int drejtimi;
+    private int viti;
+
+
+    public static Studenti fromResultSet(ResultSet res) {
+        try {
+            int id = res.getInt("Id");
+            String emri = res.getString("Emri");
+            String mbiemri = res.getString("Mbiemri");
+            String studentId = res.getString("StudentId");
+            int drejtimi = res.getInt("Drejtimi");
+            int viti = res.getInt("Viti");
+            return new Studenti(id,emri,mbiemri,studentId,drejtimi,viti);
+        }catch(SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Studenti createStudenti(
+            int id, String emri, String mbiemri,String studentId, int drejtimi, int viti
+    ) {
+        return new Studenti(id,emri,mbiemri,studentId,drejtimi,viti;
+    }
+
+    public Studenti(int id, String emri, String mbiemri,String studentId, int drejtimi, int viti) {
+        this.id = id;
+        this.emri = emri;
+        this.mbiemri = mbiemri;
+        this.studentId = studentId;
+        this.drejtimi = drejtimi;
+        this.viti = viti;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
+
+    public String getMbiemri() {
+        return mbiemri;
+    }
+
+    public void setMbiemri(String mbiemri) {
+        this.mbiemri = mbiemri;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getDrejtimi() {
+        return drejtimi;
+    }
+
+    public void setDrejtimi(int drejtimi) {
+        this.drejtimi = drejtimi;
+    }
+
+    public int getViti() {
+        return viti;
+    }
+
+    public void setViti(int viti) {
+        this.viti = viti;
+    }
+
+
+
 }
---
+++ src/main/java/repository/DrejtimiRepository.java
@@ -0,0 +1,69 @@
+package repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.DBConnection;
+import model.Drejtimi;
+
+public class DrejtimiRepository {
+
+    private DBConnection connection;
+
+    public DrejtimiRepository() {
+        this.connection = DBConnection.getConnection();
+    }
+
+    public ArrayList<Drejtimi> findAll() throws SQLException{
+        String query = "SELECT * FROM DREJTIMI";
+        ResultSet res = this.connection.executeQuery(query);
+        ArrayList<Drejtimi> drejtimi = new ArrayList<Drejtimi>();
+
+        while(res.next()) {
+            drejtimi.add(Drejtimi.fromResultSet(res));
+        }
+        res.close();
+        return drejtimi;
+    }
+
+    public String[] getDrejtimet() throws SQLException {
+        ArrayList<Drejtimi> d = findAll();
+
+        String[] drejtimet = new String[d.size()];
+
+        for (int i = 0; i < d.size(); i++) {
+            drejtimet[i] = d.get(i).getEmri();
+        }
+
+        return drejtimet;
+    }
+
+    public int getIdByEmri(String drejtimi) throws SQLException {
+        String query = "SELECT * FROM DREJTIMI WHERE EMRI = '" + drejtimi + "'";
+        ResultSet res = this.connection.executeQuery(query);
+        res.next();
+        int i = Drejtimi.fromResultSet(res).getId();
+        res.close();
+        return i;
+    }
+
+    public String getEmriById(int id) throws SQLException {
+        String query = "SELECT * FROM DREJTIMI WHERE ID = " + id;
+        ResultSet res = this.connection.executeQuery(query);
+        res.next();
+        String s = Drejtimi.fromResultSet(res).getEmri();
+        res.close();
+        return s;
+    }
+
+    public String getEmriBySId(String id) throws SQLException {
+        String query = "SELECT * FROM DREJTIMI WHERE ID = " + id;
+        ResultSet res = this.connection.executeQuery(query);
+        res.next();
+        String s = Drejtimi.fromResultSet(res).getEmri();
+        res.close();
+        return s;
+    }
+
+}
