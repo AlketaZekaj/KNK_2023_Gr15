d2d6298d72f340004ae3aa667df08fce745a9dd1
Ardit Haklaj <118832945+ArditHaklaj@users.noreply.github.com>
2023-05-24 16:35:55 +0200

    Update module-info.java

--- KNK-projekti-main/module-info.java
+++ KNK-projekti-main/module-info.java
@@ -1,1 +1,19 @@
+module KNK {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires transitive javafx.graphics;
+	requires transitive java.sql;
+    requires mysql.connector.j;
 
+    opens Application to javafx.graphics, javafx.fxml;
+	exports Application;
+	opens Controllerat to javafx.graphics, javafx.fxml;
+	exports Controllerat;
+	opens Modelet to javafx.graphics, javafx.fxml;
+	exports Modelet;
+	opens repository to javafx.graphics, javafx.fxml;
+	exports repository;
+	
+	
+}
